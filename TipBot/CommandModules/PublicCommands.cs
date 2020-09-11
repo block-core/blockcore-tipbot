daa092edc9b99f6c3cf7a06ab7d0d647889f4b2e
Mud <44410798+MudDev@users.noreply.github.com>
2020-09-11 06:49:21 -0600

    Updated Blockcore URL and logo

--- TipBot/CommandModules/PublicCommands.cs
+++ TipBot/CommandModules/PublicCommands.cs
@@ -460,7 +460,7 @@ namespace TipBot.CommandModules
 
             Version version = Assembly.GetExecutingAssembly().GetName().Version;
 
-            string text = "`TipBot`" + Environment.NewLine + $"Version: {version}" + Environment.NewLine + "Github: <https://github.com/MudDev/DiscordTipBot>";
+            string text = "`TipBot`" + Environment.NewLine + $"Version: {version}" + Environment.NewLine + "Github: <https://github.com/block-core/Blockcore-DiscordTipBot>";
 
             this.logger.Trace("(-)");
             return this.Context.Channel.SendFileAsync(stream, "logo.png", text);
